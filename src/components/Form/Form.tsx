9806b837beefdb715de4f82dec8340f1e3291b6f
thatanjan <thatanjan@gmail.com>
2022-06-23 20:59:56 +0600

    All file import errors are resolved with absolute import

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,69 +1,79 @@
-import * as React from "react";
-import { Button } from "./button";
-import styles from "./form.module.css";
+import * as React from 'react'
+
+import { Button } from 'components/Button/Button'
+
+import styles from './Form.module.css'
 
 type IFormProps = {
-  "on-submit": (payload: { title: string; description: string; price: string }) => void;
+	'on-submit': (payload: {
+		title: string
+		description: string
+		price: string
+	}) => void
 }
 
-export const Form: React.FC<IFormProps> = (props) => {
-  let formRef = React.useRef<HTMLFormElement>(null);
-  let titleRef = React.useRef<HTMLInputElement>(null);
-  let priceRef = React.useRef<HTMLInputElement>(null);
-  let descriptionRef = React.useRef<HTMLTextAreaElement>(null);
+export const Form: React.FC<IFormProps> = props => {
+	let formRef = React.useRef<HTMLFormElement>(null)
+	let titleRef = React.useRef<HTMLInputElement>(null)
+	let priceRef = React.useRef<HTMLInputElement>(null)
+	let descriptionRef = React.useRef<HTMLTextAreaElement>(null)
 
-  const handleSubmit = (e: any) => {
-    e.preventDefault();
+	const handleSubmit = (e: any) => {
+		e.preventDefault()
 
-    if (!titleRef.current?.value) {
-      alert("Your product needs a title");
+		if (!titleRef.current?.value) {
+			alert('Your product needs a title')
 
-      return;
-    }
+			return
+		}
 
-    if (!descriptionRef.current?.value || !priceRef.current?.value) {
-      alert("Your product needs some content");
+		if (!descriptionRef.current?.value || !priceRef.current?.value) {
+			alert('Your product needs some content')
 
-      return;
-    }
+			return
+		}
 
-    props["on-submit"]({
-      title: titleRef.current && titleRef.current.value,
-      description: descriptionRef.current && descriptionRef.current.value,
-      price: priceRef.current && priceRef.current.value,
-    });
+		props['on-submit']({
+			title: titleRef.current && titleRef.current.value,
+			description: descriptionRef.current && descriptionRef.current.value,
+			price: priceRef.current && priceRef.current.value,
+		})
 
-    formRef.current?.reset();
-  };
+		formRef.current?.reset()
+	}
 
-  return (
-    <form className={styles.form} onSubmit={(event) => handleSubmit(event)} ref={formRef}>
-      <span className={styles.label}>Product title: *</span>
+	return (
+		<form
+			className={styles.form}
+			onSubmit={event => handleSubmit(event)}
+			ref={formRef}
+		>
+			<span className={styles.label}>Product title: *</span>
 
-      <input
-        ref={titleRef}
-        placeholder="Title..."
-        defaultValue=""
-        className={styles.input}
-      />
+			<input
+				ref={titleRef}
+				placeholder='Title...'
+				defaultValue=''
+				className={styles.input}
+			/>
 
-      <span className={styles.label}>Product details: *</span>
+			<span className={styles.label}>Product details: *</span>
 
-      <input
-        ref={priceRef}
-        placeholder="Price..."
-        defaultValue=""
-        className={styles.input}
-      />
+			<input
+				ref={priceRef}
+				placeholder='Price...'
+				defaultValue=''
+				className={styles.input}
+			/>
 
-      <textarea
-        ref={descriptionRef}
-        placeholder="Start typing product description here..."
-        defaultValue=""
-        className={styles.textarea}
-      />
+			<textarea
+				ref={descriptionRef}
+				placeholder='Start typing product description here...'
+				defaultValue=''
+				className={styles.textarea}
+			/>
 
-      <Button>Add a product</Button>
-    </form>
-  );
-};
+			<Button>Add a product</Button>
+		</form>
+	)
+}
