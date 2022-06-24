d23025b02a63aeee1c0c5e623e55e26537dea490
thatanjan <thatanjan@gmail.com>
2022-06-24 09:29:33 +0600

    Type: Bug fix
    Desc: Correct Header styles file is imported.

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
-import styles from 'App.module.css'
-
 import logo from 'images/droppe-logo.png'
+
+import styles from './Header.module.css'
 
 const Header = () => (
 	<div className={styles.header}>
