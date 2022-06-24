ed3cf020ec33e1dc3a0523a436e4bed5f413c09f
thatanjan <thatanjan@gmail.com>
2022-06-24 11:30:39 +0600

    button component is exported as default

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -6,8 +6,10 @@ interface props {
 	onClick?: () => void
 }
 
-export const Button: React.FC<props> = ({ children, onClick }) => (
+const Button: React.FC<props> = ({ children, onClick }) => (
 	<button className={styles.button} onClick={onClick}>
 		{children}
 	</button>
 )
+
+export default Button
--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 
-import { Button } from 'components/Button/Button'
+import Button from 'components/Button/Button'
 
 import styles from './Form.module.css'
 
