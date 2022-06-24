32e71f4212e393d2c0b65d43f75d1409f268e480
thatanjan <thatanjan@gmail.com>
2022-06-24 13:49:42 +0600

    addProductPayload interface name is capitalized

--- src/types/product.ts
+++ src/types/product.ts
@@ -1,11 +1,11 @@
-export interface addProductPayload {
+export interface AddProductPayload {
 	title: string
 	description: string
 	price: string
 }
 
-export type AddProduct = (_: addProductPayload) => void
+export type AddProduct = (_: AddProductPayload) => void
 
-export interface Product extends addProductPayload {
+export interface Product extends AddProductPayload {
 	isFavorite: boolean
 }
