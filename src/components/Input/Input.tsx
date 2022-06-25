2fa617ae76d202fc26d824e1706e21a8d2ce7a24
thatanjan <thatanjan@gmail.com>
2022-06-25 17:04:58 +0600

    input tag is replaced with Field component

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,4 +1,5 @@
 import React, { InputHTMLAttributes, LabelHTMLAttributes } from 'react'
+import { Formik, Form, Field, ErrorMessage, ErrorMessageProps } from 'formik'
 
 import styles from './Input.module.css'
 
@@ -7,18 +8,21 @@ interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
 	id: string
 	labelProps?: LabelHTMLAttributes<HTMLLabelElement>
 	value: string | number
+	errorMessageProps?: ErrorMessageProps
 }
 
-const Input = ({ labelProps, id, ...props }: InputProps) => (
+const Input = ({ labelProps, ...props }: InputProps) => (
 	<div>
-		<label className={styles.label} htmlFor={id} {...labelProps} />
-		<input className={styles.input} name={id} {...props} />
+		<label className={styles.label} htmlFor={props.id} {...labelProps} />
+		<Field className={styles.input} name={props.id} {...props} />
+		<ErrorMessage name={props.id} component='div' />
 	</div>
 )
 
 Input.defaultProps = {
 	type: 'text',
 	labelProps: {},
+	errorMessageProps: {},
 }
 
 export default Input
