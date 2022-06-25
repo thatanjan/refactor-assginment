48816a4944cf7bd93d90551389d31acf1dfaf111
thatanjan <thatanjan@gmail.com>
2022-06-25 15:11:29 +0600

    1. App component is converted to functional component 2. All the unnecessary codes are removed.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
-import * as React from 'react'
-import lodash from 'lodash'
+import React from 'react'
 
 import Header from 'components/Header/Header'
 import DisplayProducts from 'components/DisplayProducts/DisplayProducts'
@@ -31,106 +30,14 @@ const DisplayImage = () => (
 	</section>
 )
 
-class App extends React.Component<
-	{},
-	{
-		products: any[]
-		isOpen: boolean
-		isShowingMessage: boolean
-		message: string
-		numFavorites: number
-		prodCount: number
-	}
-> {
-	constructor(props: any) {
-		super(props)
+const App = () => (
+	<main className={styles.container}>
+		<Header />
 
-		this.favClick = this.favClick.bind(this)
-		this.onSubmit = this.onSubmit.bind(this)
+		<DisplayImage />
 
-		this.state = {
-			products: [],
-			isOpen: false,
-			isShowingMessage: false,
-			message: '',
-			numFavorites: 0,
-			prodCount: 0,
-		}
-	}
-
-	favClick(title: string) {
-		const prods = this.state.products
-		const idx = lodash.findIndex(prods, { title: title })
-		let currentFavs = this.state.numFavorites
-		let totalFavs: any
-
-		if (prods[idx].isFavorite) {
-			prods[idx].isFavorite = false
-			totalFavs = --currentFavs
-		} else {
-			totalFavs = ++currentFavs
-			prods[idx].isFavorite = true
-		}
-
-		this.setState(() => ({ products: prods, numFavorites: totalFavs }))
-	}
-
-	onSubmit(payload: { title: string; description: string; price: string }) {
-		const updated = lodash.clone(this.state.products)
-		updated.push({
-			title: payload.title,
-			description: payload.description,
-			price: payload.price,
-		})
-
-		this.setState({
-			products: updated,
-			prodCount: lodash.size(this.state.products) + 1,
-		})
-
-		this.setState({
-			isOpen: false,
-		})
-
-		this.setState({
-			isShowingMessage: true,
-			message: 'Adding product...',
-		})
-
-		// **this POST request doesn't actually post anything to any database**
-		fetch('https://fakestoreapi.com/products', {
-			method: 'POST',
-			body: JSON.stringify({
-				title: payload.title,
-				price: payload.price,
-				description: payload.description,
-			}),
-		})
-			.then(res => res.json())
-			.then(json => {
-				;(function (t) {
-					setTimeout(() => {
-						t.setState({
-							isShowingMessage: false,
-							message: '',
-						})
-					}, 2000)
-				})(this)
-			})
-	}
-
-	render() {
-		const { products, isOpen } = this.state
-		return (
-			<main className={styles.container}>
-				<Header />
-
-				<DisplayImage />
-
-				<DisplayProducts />
-			</main>
-		)
-	}
-}
+		<DisplayProducts />
+	</main>
+)
 
 export default App
