30bb6c3519ef651d49b3dc1faca70e23cdc32bb4
thatanjan <thatanjan@gmail.com>
2022-06-25 22:54:58 +0600

    Product action button is refactored and restyled

--- src/components/Product/ProductList.tsx
+++ src/components/Product/ProductList.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import { FaStar } from 'react-icons/fa'
 import { nanoid } from 'nanoid'
+import clsx from 'clsx'
 
 import { Product } from 'types/product'
 
@@ -26,44 +27,39 @@ const ProductCard = ({
 	description,
 	isFavorite,
 	toggleFavorite,
-}: ProductCardProps) => {
-	const { productBody, actionBarItem, actionBarItemLabel } = styles
-	return (
-		<article className={styles.product}>
-			<h2 className={styles.product__title}>{title}</h2>
+}: ProductCardProps) => (
+	<article className={styles.product}>
+		<h2 className={styles.product__title}>{title}</h2>
 
-			{rate !== undefined && (
-				<p>
-					<strong>Rating:</strong> <span> {rate}/5</span>
-				</p>
-			)}
-
+		{rate !== undefined && (
 			<p>
-				<strong>Price: </strong> <span>${price}</span>
+				<strong>Rating:</strong> <span> {rate}/5</span>
 			</p>
+		)}
 
-			<p className={styles.product__description}>
-				<strong>Description:</strong> <span>{description}</span>
-			</p>
+		<p>
+			<strong>Price: </strong> <span>${price}</span>
+		</p>
 
-			<div
-				className={styles.action_bar}
-				style={{ display: 'table', width: '100%' }}
+		<p className={styles.product__description}>
+			<strong>Description:</strong> <span>{description}</span>
+		</p>
+
+		<div className={styles.action__bar}>
+			<button
+				type='button'
+				onClick={toggleFavorite(index)}
+				className={clsx(styles.action__button, isFavorite && 'favorite')}
 			>
-				<button
-					type='button'
-					className={`${actionBarItem} ${isFavorite ? 'active' : ''}`}
-					onClick={toggleFavorite(index)}
-				>
-					<FaStar />{' '}
-					<span className={actionBarItemLabel}>
-						{isFavorite ? 'Remove from favorites' : 'Add to favorites'}
-					</span>
-				</button>
-			</div>
-		</article>
-	)
-}
+				<FaStar />
+
+				<span className={styles.action__button__label}>
+					{isFavorite ? 'Remove from favorites' : 'Add to favorites'}
+				</span>
+			</button>
+		</div>
+	</article>
+)
 
 const ProductList = ({ products, toggleFavorite }: ProductListProps) => (
 	<section className={styles.product__list}>
