0ba0c6c4b318456e6aadc3125695ef2b6d12997f
thatanjan <thatanjan@gmail.com>
2022-06-25 13:39:20 +0600

    Button component is refactored

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,13 +1,10 @@
-import * as React from 'react'
+import React, { DOMAttributes } from 'react'
 import styles from './Button.module.css'
 
-interface props {
-	children: any
-	onClick?: () => void
-}
+type ButtonProps = DOMAttributes<HTMLButtonElement>
 
-const Button: React.FC<props> = ({ children, onClick }) => (
-	<button className={styles.button} onClick={onClick}>
+const Button = ({ children, onClick }: ButtonProps) => (
+	<button type='button' className={styles.button} onClick={onClick}>
 		{children}
 	</button>
 )
